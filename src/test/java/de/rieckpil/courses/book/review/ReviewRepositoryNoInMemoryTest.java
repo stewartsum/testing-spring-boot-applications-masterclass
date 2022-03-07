a79cd150d54235abefc0bc652321fb021036404d
Stewart Sum <stewartsum@users.noreply.github.com>
2022-03-07 04:51:02 -0700

    Testing Database Access > Replacing The In-Memory Database >> Using Testcontainers for the First Time

--- src/test/java/de/rieckpil/courses/book/review/ReviewRepositoryNoInMemoryTest.java
+++ src/test/java/de/rieckpil/courses/book/review/ReviewRepositoryNoInMemoryTest.java
@@ -1,15 +1,25 @@
 package de.rieckpil.courses.book.review;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
 @Testcontainers(disabledWithoutDocker = true)
@@ -30,7 +40,60 @@ class ReviewRepositoryNoInMemoryTest {
   }
 
   @Autowired
+  private EntityManager entityManager;
+
+  @Autowired
   private ReviewRepository cut;
+
+  @Autowired
+  private DataSource dataSource;
+
+  @Autowired
+  private TestEntityManager testEntityManager;
+
+  @BeforeEach
+  void beforeEach() {
+    assertEquals(0, cut.count());
+  }
+
+  @Test
+  void notNull() throws SQLException {
+
+    assertNotNull(entityManager);
+    assertNotNull(cut);
+    assertNotNull(testEntityManager);
+    assertNotNull(dataSource);
+
+    System.out.println("Database Product Name: " + dataSource.getConnection().getMetaData().getDatabaseProductName()); // Database Product Name: H2
+
+    Review review = new Review();
+    review.setContent("Duke");
+    review.setTitle("Review 101");
+    review.setCreatedAt(LocalDateTime.now());
+    review.setRating(5);
+    review.setBook(null);
+    review.setUser(null);
+
+    Review result = cut.save(review); // insert into reviews (id, book_id, content, created_at, rating, title, user_id) values (null, NULL, 'Good review', '2022-03-07T02:25:56.879-0700', 5, 'Review 101', NULL);
+
+    System.out.println("result: " + result); // result: Review{id=1, title='Review 101', content='Good review', rating=5, createdAt=2022-03-07T02:25:56.879116008, book=null, user=null}
+    assertNotNull(result.getId());
+  }
+
+  @Test
+  void transactionalSupportTest() {
+
+    Review review = new Review();
+    review.setContent("Duke");
+    review.setTitle("Review 101");
+    review.setCreatedAt(LocalDateTime.now());
+    review.setRating(5);
+    review.setBook(null);
+    review.setUser(null);
+
+    Review result = cut.save(review);
+
+  }
 
   @Test
   @Sql(scripts = "/scripts/INIT_REVIEW_EACH_BOOK.sql")

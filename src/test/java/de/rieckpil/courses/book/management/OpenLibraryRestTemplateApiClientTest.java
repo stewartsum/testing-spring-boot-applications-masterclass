eb94863a98970c6ab6549abbc18e3d92fd12dc24
Stewart Sum <stewartsum@users.noreply.github.com>
2022-03-09 23:26:14 -0700

    Testing Further Slices Of Our Application > Testing HTTP Clients Using The RestTemplate >> More Assertions for the Happy-Path

--- src/test/java/de/rieckpil/courses/book/management/OpenLibraryRestTemplateApiClientTest.java
+++ src/test/java/de/rieckpil/courses/book/management/OpenLibraryRestTemplateApiClientTest.java
@@ -9,11 +9,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.test.web.client.match.MockRestRequestMatchers;
 import org.springframework.test.web.client.response.MockRestResponseCreators;
-import org.springframework.web.client.HttpServerErrorException;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 @RestClientTest(OpenLibraryRestTemplateApiClient.class)
 class OpenLibraryRestTemplateApiClientTest {
@@ -36,14 +33,23 @@ class OpenLibraryRestTemplateApiClientTest {
   void shouldReturnBookWhenResultIsSuccess() {
 
     this.mockRestServiceServer
-      .expect(MockRestRequestMatchers.requestTo("/api/books?jscmd=data&format=json&bibkeys=ISBN:" + ISBN))
+      .expect(MockRestRequestMatchers.requestTo(Matchers.containsString(ISBN)))
+      //.expect(MockRestRequestMatchers.requestTo("/api/books?jscmd=data&format=json&bibkeys=ISBN:" + ISBN))
       .andRespond(MockRestResponseCreators.withSuccess(new ClassPathResource("/stubs/openlibrary/success-" + ISBN + ".json"),
         MediaType.APPLICATION_JSON));
 
     Book result = cut.fetchMetadataForBook(ISBN);
 
-    assertNotNull(result);
+    assertEquals("9780596004651", result.getIsbn());
+    assertEquals("Head first Java", result.getTitle());
+    assertEquals("https://covers.openlibrary.org/b/id/388761-S.jpg", result.getThumbnailUrl());
+    assertEquals("Kathy Sierra", result.getAuthor());
+    assertEquals("Your brain on Java--a learner's guide--Cover.Includes index.", result.getDescription());
+    assertEquals("Java (Computer program language)", result.getGenre());
+    assertEquals("O'Reilly", result.getPublisher());
+    assertEquals(619, result.getPages());
 
+    assertNull(result.getId());
   }
 
   @Test

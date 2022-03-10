9d03ab4661e724657f60b99a64dfd8bcf602498b
Stewart Sum <stewartsum@users.noreply.github.com>
2022-03-10 02:01:48 -0700

    Testing Further Slices Of Our Application > Testing HTTP Clients Using The WebClient >> Required Setup for the MockWebServer

--- src/test/java/de/rieckpil/courses/book/management/OpenLibraryApiClientTest.java
+++ src/test/java/de/rieckpil/courses/book/management/OpenLibraryApiClientTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
+import reactor.netty.tcp.TcpClient;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
@@ -20,6 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static reactor.netty.tcp.TcpClient.*;
 
 class OpenLibraryApiClientTest {
 
@@ -30,8 +32,30 @@ class OpenLibraryApiClientTest {
 
   private static String VALID_RESPONSE;
 
+  @BeforeEach
+  public void setup() throws IOException {
+
+    TcpClient tcpClient = create()
+      .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 1_000)
+      .doOnConnected(connection ->
+        connection.addHandlerLast(new ReadTimeoutHandler(1))
+          .addHandlerLast(new WriteTimeoutHandler(1)));
+
+    this.mockWebServer = new MockWebServer();
+    this.mockWebServer.start();
+
+    this.cut = new OpenLibraryApiClient(
+      WebClient.builder()
+        .clientConnector(new ReactorClientHttpConnector(HttpClient.from(tcpClient)))
+        .baseUrl(mockWebServer.url("/").toString())
+        .build()
+    );
+  }
+
   @Test
   void notNull() {
+    assertNotNull(cut);
+    assertNotNull(mockWebServer);
   }
 
   @Test
